7b2b9a7d2950ca2be1950ec8981f2b7644c1a9ea
Avinash Madhwani <avinashmadhwani02@gmail.com>
2026-08-29 00:39:43 +0530

    added docker compose

--- apps/medusa/medusa-config.ts
+++ apps/medusa/medusa-config.ts
@@ -2,6 +2,8 @@ import { loadEnv, defineConfig } from '@medusajs/framework/utils'
 
 loadEnv(process.env.NODE_ENV || 'development', process.cwd())
 
+const isProd = process.env.NODE_ENV === 'production'
+
 module.exports = defineConfig({
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
@@ -11,6 +13,34 @@ module.exports = defineConfig({
       authCors: process.env.AUTH_CORS!,
       jwtSecret: process.env.JWT_SECRET,
       cookieSecret: process.env.COOKIE_SECRET,
-    }
-  }
+    },
+  },
+  ...(isProd
+    ? {
+        modules: [
+          {
+            resolve: '@medusajs/medusa/file',
+            options: {
+              providers: [
+                {
+                  resolve: '@medusajs/medusa/file-s3',
+                  id: 's3',
+                  options: {
+                    file_url: process.env.S3_FILE_URL,
+                    access_key_id: process.env.S3_ACCESS_KEY_ID,
+                    secret_access_key: process.env.S3_SECRET_ACCESS_KEY,
+                    region: process.env.S3_REGION,
+                    bucket: process.env.S3_BUCKET,
+                    endpoint: process.env.S3_ENDPOINT,
+                    ...(process.env.S3_FORCE_PATH_STYLE === 'true'
+                      ? { additional_client_config: { forcePathStyle: true } }
+                      : {}),
+                  },
+                },
+              ],
+            },
+          },
+        ],
+      }
+    : {}),
 })
